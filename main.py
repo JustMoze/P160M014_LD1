e4e9d8984d4388b3c1294d80e05589b65b9d76c9
Modestas Rimeikis <modestas.rimeikis@telesoftas.com>
2023-10-04 18:56:33 +0300

    Drop dublicates from DF

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ import pandas as pd
 
 creditRiskDF = pd.read_csv('credit_risk.csv', index_col="Id")
 creditRiskDF.info()
+
+# Dropping duplicate values 
+creditRiskDF.drop_duplicates(inplace=True)
+creditRiskDF = creditRiskDF.reset_index(drop=True)
+creditRiskDF.shape
+
+creditRiskDF.describe
